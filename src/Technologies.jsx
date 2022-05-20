bc2808d7bc83468519be8a56a3c380d6baf4fdfa
Aleksandr_UB <bogdanov777000@mail.ru>
2022-05-20 23:03:41 +0300

    first commit

---
+++ src/Technologies.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+
+function Technologies() {
+    return (
+        <div>
+            <ul>
+                <li>css</li>
+                <li>html</li>
+                <li>js</li>
+                <li>react</li>
+            </ul>
+        </div>
+    )
+}
+export  default Technologies;
