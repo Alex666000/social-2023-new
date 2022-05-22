78bcbbeb4737f7ee9ed62c0a582343502accb775
Aleksandr_UB <bogdanov777000@mail.ru>
2022-05-22 13:43:52 +0300

    10 begin

--- src/Technologies.tsx
+++ src/Technologies.tsx
@@ -5,10 +5,10 @@ function Technologies() {
     return (
         <div>
             <ul>
-                <li>css</li>
-                <li>html</li>
-                <li>js</li>
-                <li>react</li>
+                <li>--css</li>
+                <li>--html</li>
+                <li>--js</li>
+                <li>--react</li>
             </ul>
         </div>
     )
