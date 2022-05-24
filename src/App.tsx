d81f0d1966230f03b8cb58392c15ebdd8c3f9b6a
Aleksandr_UB <bogdanov777000@mail.ru>
2022-05-24 20:26:12 +0300

    11 css grig ready

--- src/App.tsx
+++ src/App.tsx
@@ -3,15 +3,51 @@ import './App.css';
 
 const App: React.FC = () => {
     return (
-        <div>
-            <header>
-                <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSzpExVyX0u7sbSBpSxVdui50mn1_slr9JXTw&usqp=CAU" alt="react-logo"/>
+        <div className={'app-wrapper'}>
+            <header className={'header'}>
+                <img
+                    src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRf8yndOQhIaFf36hkWn9w6UAff4nmPREbmvQ&usqp=CAU'
+                    alt="react-logo"/>
             </header>
-            <nav>
-                <div>Profile</div>
-                <div>Messages</div>
-                <div>Main content</div>
+            <nav className={'nav'}>
+                <div>
+                    <a href="#">Profile</a>
+                </div>
+                <div>
+                    <a href="#">Messages</a>
+                </div>
+                <div>
+                    <a href="#">News</a>
+                </div>
+                <div>
+                    <a href="#">Music</a>
+                </div>
+                <div>
+                    <a href="#">Settings</a>
+                </div>
             </nav>
+            <div className={'content'}>
+                <div>
+                    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS2aOTrZH4dN0uKM1hMP5b4ewjeyEERCzo7kA&usqp=CAU" alt="TS"/>
+                </div>
+                <div>
+                    avatar + descripshion
+                </div>
+                <div>
+                    My post
+                    <div>
+                        New post
+                    </div>
+                    <div>
+                        <div>
+                            post 1
+                        </div>
+                        <div>
+                            post 2
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 }
