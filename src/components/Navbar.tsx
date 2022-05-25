5312f4104b0b09afc1f36cea4dedf8e0c85dbfb4
Aleksandr_UB <bogdanov777000@mail.ru>
2022-05-25 16:10:49 +0300

    14 full ready module css

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,29 +1,34 @@
 import React from 'react';
-import s from './Navbar.module.css'
+import s from './Navbar.module.css';
+// let s = {
+//     'nav': 'Navbar_nav__3ou9Q',
+//     'item': 'Navbar_item__3qaF3',
+//     'active' : 'Baksndakdn_actve'
 
-console.log(s)
+// let c1 = "item";
+// let c2 = "active";
+// // "item active"
+// let classes = c1 + " " + c2;
+// let classesNew = `${s.item} ${c2}`;
 
-const Navbar: React.FC = () => {
-    return (
-        <nav className={s.nav}>
-            <div className={s.item}>
-                <a href="src/components/Navbar#">Profile</a>
-            </div>
-            {/*ставим класс item active*/}
-            <div className={` ${s.item} ${s.active} `}>
-                <a href="src/components/Navbar#">Messages</a>
-            </div>
-            <div className={s.item}>
-                <a href="src/components/Navbar#">News</a>
-            </div>
-            <div className={s.item}>
-                <a href="src/components/Navbar#">Music</a>
-            </div>
-            <div className={s.item}>
-                <a href="src/components/Navbar#">Settings</a>
-            </div>
-        </nav>
-    );
-};
+const Navbar = () => {
+    return <nav className={s.nav}>
+        <div className={s.item}>
+            <a>Profile</a>
+        </div>
+        <div className={`${s.item} ${s.active}`}>
+            <a>Messages</a>
+        </div>
+        <div className={s.item}>
+            <a>News</a>
+        </div>
+        <div className={s.item}>
+            <a>Music</a>
+        </div>
+        <div className={s.item}>
+            <a>Settings</a>
+        </div>
+    </nav>
+}
 
 export default Navbar;
