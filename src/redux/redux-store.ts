0533e972b343226d9a99706f0f7cb7bf6073b6d7
Aleksandr_UB <bogdanov777000@mail.ru>
2022-07-05 10:22:18 +0300

    42 - create store

--- src/redux/redux-store.ts
+++ src/redux/redux-store.ts
@@ -1,4 +1,13 @@
-import {createStore} from 'redux';
+import {combineReducers, createStore} from 'redux';
+import {profileReduser} from './profile-reduser';
+// объединяем все редюсеры: в эту функцию передаем объект внутри
+let reducers = combineReducers({
+    // ветки - за ветки отвечают эти редюсеры
+    profilePage: profileReduser,
+    dialogsPage: profileReduser,
+    sidebar: profileReduser,
+})
+// "закомбайненные" редюссеры отдаем в store, автоматически createStore создает внутри себя state с его свойствами:
+let store = createStore(reducers)
 
-
-let store = createStore()
+export default store
