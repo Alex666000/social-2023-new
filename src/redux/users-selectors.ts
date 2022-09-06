7c9b2f742e0a3b7f9a4408d9ec863dde360d41d3
Aleksandr_UB <bogdanov777000@mail.ru>
2022-09-06 17:25:05 +0300

    83 - библиотека reselect

--- src/redux/users-selectors.ts
+++ src/redux/users-selectors.ts
@@ -1,11 +1,22 @@
 import {AppRootStateType} from './redux-store';
 import {IUser} from './users-reducer';
+import {createSelector} from 'reselect';
 // Селектор: выбирает, забирает или вытягивает данные
 // если users не изменились, то не будем перерисовывать целевую компоненту и не будем вызывать даже селектор - если селектор вызовется, то всегда образуется новый объект! filter, map - тоже создает новый объект,
-
-export const getUsers = (state: AppRootStateType): IUser[] => {
+// МЫ ХОТЕЛИ БЫ ВЫЗЫВАТЬ НАШУФУНКЦИЮ СТОЛЬКО РАЗ СКОЛЬКО ОНА БУДЕТ ВЫЗЫВАТЬСЯ, НО ЧТОБЫ ФУНКЦИЯ НЕ ПЕРЕЗАПУСКАЛАСЬ ЕСЛИ В ЭТОМ НЕТ СМЫСЛА - Библиотека Reselect...
+// ---------------------------------------------------------------------------------
+// примитивный селектор
+export const getUsersSelector = (state: AppRootStateType): IUser[] => {
     return state.usersPage.users
 }
+// внутри реселекта записана сложная логика по сохранению значения по оценки не изменились ли входные данные...
+// более сложный селектор использует более примитивный, чтобы получить, то что нужно более сложному селектору
+// результат работы примитивного селектора: users - засовывает в свою колбек-функцию
+// если state меняется, но не будет меняться в state_е: users, то функция-колбек не будет перезапускаться
+export const getUsers = createSelector(getUsersSelector,
+    (users) => {
+        return users.filter(u => true);
+    })
 
 export const getPageSize = (state: AppRootStateType): number => {
     return state.usersPage.pageSize
